e9e2b8ce6fc2be5ce4ff20385beda02b6ce74409
KevinFlores19 <145875283+KevinFlores19@users.noreply.github.com>
2025-04-19 22:41:37 -0600

    Completed IA section and started with the Apps Section

---
+++ Blog_tecnologico/noticias/iA/noticia5/alexa+.js
@@ -0,0 +1,8 @@
+// Sección para menu responsivo
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+// Alternar clase activa al hacer clic en el menú
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
